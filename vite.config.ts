f04800e1bfd98e78214b45e520340459199bce85
Stanisav Karnoza <s.karnoza@joinposter.com>
2025-07-29 13:03:14 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import {type ManifestOptions, VitePWA} from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+
+const manifest: Partial<ManifestOptions> | false = {
+    "theme_color": "#8936FF",
+    "background_color": "#2EC6FE",
+    "icons": [
+        {
+            "purpose": "maskable",
+            "sizes": "512x512",
+            "src": "icon512_maskable.png",
+            "type": "image/png"
+        },
+        {
+            "purpose": "any",
+            "sizes": "512x512",
+            "src": "icon512_rounded.png",
+            "type": "image/png"
+        }
+    ],
+    screenshots: [
+        {
+            src: "/screenshots/destop.png",
+            type: "image/png",
+            sizes: "1444x745",
+            form_factor: "wide"
+        },
+        {
+            src: "/screenshots/mobile.png",
+            type: "image/png",
+            sizes: "369x663",
+            form_factor: "narrow"
+        }
+    ],
+    "orientation": "any",
+    "display": "standalone",
+    "lang": "ru-RU",
+    "name": "Umamiq",
+    "short_name": "UMIQ"
+}
+
+
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ["**/*.{html,css,js,ico,png,svg}"]
+            },
+            manifest
+        })
+    ],
+    server: {
+        allowedHosts: ['9b697a5d7aa3.ngrok-free.app'],
+    }
+})
